4d1d156830e51f1d5e3d6dd0b8df0efd3ba26e6e
swalker2m <swalker2m@mac.com>
2015-08-18 15:39:24 -0300

    Split the properties into two columns.

--- bundle/jsky.app.ot/src/main/scala/jsky/app/ot/editor/sequence/SequenceEditor.scala
+++ bundle/jsky.app.ot/src/main/scala/jsky/app/ot/editor/sequence/SequenceEditor.scala
@@ -6,7 +6,6 @@ import edu.gemini.spModel.sequence.sp.SequenceDO
 import jsky.app.ot.editor.OtItemEditor
 
 import java.awt.Font
-import java.beans.PropertyEditor
 import javax.swing.{JPanel, BorderFactory}
 
 import scala.swing.{Insets, GridBagPanel, Label}
@@ -83,21 +82,40 @@ class SequenceEditor extends OtItemEditor[ISPSequence, SequenceDO] {
   }
 
   private def init[I: Describe](s: Sequence[I], ctor: Sequence[I] => InstrumentSequence): Unit = {
+    // All the property sections (one for the instrument, one for telescope, etc.
+    // according to the contents of the sequence).
     val sections = PropSection.sections(s, Set(0), (seq: Sequence[I]) => {
       val dob = getDataObject
       dob.seq = Some(ctor(seq))
       apply()
     })
 
-    val rows = sections.list.flatMap { s =>
-      val title = s.props.headOption.flatMap(_.meta.attrs.label.parent).map(TitleRow)
-      val props = s.editors.map(PropRow)
-
-      title.fold(props: List[Row]) { _ :: props }
+    // Will split the properties in each section into two columns, but only if
+    // there are at least 4 properties.
+    def splitPos(ps: PropSection[_, I]): Int = {
+      val size = ps.props.size
+      if (size < 4) size else (size / 2) + (size % 2)
     }
 
-    rows.zipWithIndex.foreach { case (row, i) =>
-        row.layout(pan, i, 0)
+    // Pair each property section with the row index where it should start.
+    val sectionStartRow = sections.list.zip(sections.list.scanLeft(0) { (s, ps) =>
+      s + 1 + splitPos(ps)
+    })
+
+    // Layout the properties with a title, split properties in two columns.
+    sectionStartRow.foreach { case (ps, row) =>
+      ps.props.headOption.flatMap(_.meta.attrs.label.parent).map(TitleRow).foreach {
+        _.layout(pan, row, 0)
+      }
+
+      val propRows     = ps.editors.map(PropRow)
+      val (col0, col1) = propRows.splitAt(splitPos(ps))
+
+      def layout(props: List[PropRow], col: Int): Unit =
+        props.zipWithIndex.foreach { case (r,i) => r.layout(pan, row + i + 1, col) }
+
+      layout(col0, 0)
+      layout(col1, 3)
     }
   }
 
